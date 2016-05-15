7b6ecb1b9771572231c3be93dbb7f4347da590de
Ivan Lavriv <lavriv92@gmail.com>
2016-05-15 16:42:13 +0300

    Added methods to update and delete classes

--- lib/rom/arangodb/commands/delete.rb
+++ lib/rom/arangodb/commands/delete.rb
@@ -4,6 +4,14 @@ module ROM
   module Arangodb
     module Commands
       class Update < ROM::Commnds::Update
+        adapter :arangodb
+
+        def collection
+          relation.dataset
+        end
+
+        def execute(criteria)
+        end
       end
     end
   end
--- lib/rom/arangodb/commands/update.rb
+++ lib/rom/arangodb/commands/update.rb
@@ -4,6 +4,14 @@ module ROM
   module Arangodb
     module Commands
       class Update < ROM::Commnds::Update
+        adapter :arangodb
+
+        def collection
+          relation.dataset
+        end
+
+        def execute
+        end
       end
     end
   end
