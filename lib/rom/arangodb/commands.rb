722be24ec3a4e4e5d80ddab8c092f39616969a53
Ivan Lavriv <lavriv92@gmail.com>
2016-05-15 16:39:26 +0300

    Added update and delete commnds classes

--- lib/rom/arangodb/commands.rb
+++ lib/rom/arangodb/commands.rb
@@ -1,1 +1,3 @@
 require 'rom/arangodb/commands/create'
+require 'rom/arangodb/commands/update'
+require 'rom/arangodb/commands/delete'
---
+++ lib/rom/arangodb/commands/delete.rb
@@ -0,0 +1,10 @@
+require 'rom/commands'
+
+module ROM
+  module Arangodb
+    module Commands
+      class Update < ROM::Commnds::Update
+      end
+    end
+  end
+end
